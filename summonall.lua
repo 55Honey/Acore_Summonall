3eeff27a2ccf744b97c2acf0910fa273a069e079
55Honey <71938210+55Honey@users.noreply.github.com>
2022-02-10 22:53:17 +0100

    feat: add chat-handler support, refactor

--- summonall.lua
+++ summonall.lua
@@ -34,29 +34,32 @@ local function has_value (tab, val)
 	return false
 end
 
-local function summonAll(event, player, command)
-  if command == 'summonall' then
-	mapId = player:GetMapId()
-	--allow to proceed if the player is on one of the maps listed above
-	if has_value(maps, mapId) then
-		--allow to proceed if the player is not in combat
-		if not player:IsInCombat() then
-			group = player:GetGroup()
-			groupPlayers = group:GetMembers()
-			for _, v in pairs(groupPlayers) do
-				if v ~= player then
-					v:SummonPlayer(player)
-				end	
+local function summonAll(event, player, command, chatHandler)
+	if command == 'summonall' then
+		if player == nil then
+			chatHandler:SendSysMessage('summonall can not be used from the console.')
+		end
+		mapId = player:GetMapId()
+		--allow to proceed if the player is on one of the maps listed above
+		if has_value(maps, mapId) then
+			--allow to proceed if the player is not in combat
+			if not player:IsInCombat() then
+				group = player:GetGroup()
+				groupPlayers = group:GetMembers()
+				for _, v in pairs(groupPlayers) do
+					if v ~= player then
+						v:SummonPlayer(player)
+					end
+				end
+			else
+				chatHandler:SendSysMessage("Summoning is not possible in combat.")
 			end
+			return false
 		else
-			player:SendBroadcastMessage("Summoning is not possible in combat.")
+			chatHandler:SendSysMessage("Summoning is not possible here.")
 		end
 		return false
-	else
-		player:SendBroadcastMessage("Summoning is not possible here.")
 	end
-	return false	
-  end
 end
 
 RegisterPlayerEvent(42, summonAll)
